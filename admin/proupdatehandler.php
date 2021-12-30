d695cb3964882969e8dc027186d5b63c5f9b3405
Ruxali Maharjan <ruxali.maharjan@kathford.edu.np>
2021-12-30 16:25:46 +0545

    updates

--- admin/proupdatehandler.php
+++ admin/proupdatehandler.php
@@ -9,16 +9,7 @@ if(isset($_POST['update']))
 	$newcat=$_POST['category'];
 
 
-$target="../uploads/";
-$file_path=$target.basename($_FILES['file']['name']);
-$file_name=$_FILES['file']['name'];
-$file_tmp=$_FILES['file']['tmp_name'];
-$file_store="../uploads/".$file_name;
-
-move_uploaded_file($file_tmp, $file_store);
-
-
-$sql="UPDATE products set name='$newname', price='$newprice', description='$newdesc', category_id='$newcat', picture='$file_path' where id='$newid' ";
+$sql="UPDATE products set name='$newname', price='$newprice', description='$newdesc', category_id='$newcat' where id='$newid' ";
 
 if(mysqli_query($connect,$sql)) {
 	echo"Updated Successfully";
